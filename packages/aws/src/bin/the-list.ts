8991c3f150c4490c1d0d4773fda3bcef8724cdc7
githop <tom@githop.com>
2024-02-19 12:17:35 -0700

    tag stack

--- packages/aws/src/bin/the-list.ts
+++ packages/aws/src/bin/the-list.ts
@@ -7,15 +7,11 @@ import { getCdkEnv } from "@the-list/env";
 const Environment = getCdkEnv(process.env);
 
 const app = new cdk.App();
-new TheListStack(app, "TheListStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  env: { account: Environment.AWS_ACCOUNT_ID, region: Environment.AWS_REGION },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const listStack = new TheListStack(app, "TheListStack", {
+  env: {
+    account: Environment.AWS_ACCOUNT_ID,
+    region: Environment.AWS_REGION,
+  },
 });
+
+cdk.Tags.of(listStack).add("name", "gth-labs/the-list");
